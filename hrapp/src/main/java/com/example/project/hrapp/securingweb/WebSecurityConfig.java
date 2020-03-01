283d8159c8f9c00ffd444d02b1eb6b05750b2863
orkunincili <orkunincili0@gmail.com>
2020-03-01 23:58:13 +0300

    Login information changed

--- hrapp/src/main/java/com/example/project/hrapp/securingweb/WebSecurityConfig.java
+++ hrapp/src/main/java/com/example/project/hrapp/securingweb/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
+                        .username("kodgemisi@kodgemisi.com")
+                        .password("kodgemisi2020")
                         .roles("USER")
                         .build();
 
